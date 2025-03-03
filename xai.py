d78adfdaa298f432e017084f72ee6ef7c22614a0
aiaccfin <aiaccfin@gmail.com>
2025-03-03 12:51:28 -0500

    g

--- xai.py
+++ xai.py
@@ -30,7 +30,8 @@ def main():
         else:
             flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             # creds = flow.run_local_server(port=0)
-            creds = flow.run_console()
+            creds = flow.run_local_server(port=0, open_browser=False)
+
         # Save the credentials for the next run
         with open("token.json", "w") as token:
             token.write(creds.to_json())
